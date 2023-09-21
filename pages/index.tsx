7b08ccf8cabf0d94e0e2e8a46b477fd24ee5a1d5
Diky Arif Febriyanto <dikyariff97@gmail.com>
2023-09-21 11:34:07 +0700

    feat: create navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
+
+import Navbar from "@/components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,16 +23,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
   return (
-    <div className="flex items-center justify-center h-full text-white font-medium text-3xl">
-      <p className="text-white">Logged in as : {user?.email}</p>
-      <button
-        onClick={() => signOut()}
-        className="px-5 py-1 rounded-md border border-white"
-      >
-        !!!
-      </button>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
